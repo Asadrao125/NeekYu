6038b6c5b553c6d12371be883865c895db682b57
asad.muqeem <raoasad125@gmail.com>
2021-12-28 15:28:09 +0500

    Initial Commit

--- app/src/main/java/com/technado/neekyu/adapters/AppsLockAdapter.kt
+++ app/src/main/java/com/technado/neekyu/adapters/AppsLockAdapter.kt
@@ -1,14 +1,18 @@
 package com.technado.neekyu.adapters
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.technado.neekyu.R
+import com.technado.neekyu.models.AppModel
 
-class AppsLockAdapter(var context: Context, var list: List<String>) :
+class AppsLockAdapter(var context: Context, var list: List<AppModel>) :
     RecyclerView.Adapter<AppsLockAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -18,7 +22,11 @@ class AppsLockAdapter(var context: Context, var list: List<String>) :
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.itemView.setOnClickListener(View.OnClickListener { })
+        holder.itemView.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "" + list.get(position).getName(), Toast.LENGTH_SHORT).show()
+        })
+        holder.appName.text = list.get(position).getName()
+        holder.image.setImageDrawable(list.get(position).getIcon())
     }
 
     override fun getItemCount(): Int {
@@ -27,9 +35,11 @@ class AppsLockAdapter(var context: Context, var list: List<String>) :
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var appName: TextView
+        var image: ImageView
 
         init {
             appName = itemView.findViewById(R.id.appName)
+            image = itemView.findViewById(R.id.image)
         }
     }
 }
--- app/src/main/java/com/technado/neekyu/fragments/home/AppsLockFragment.kt
+++ app/src/main/java/com/technado/neekyu/fragments/home/AppsLockFragment.kt
@@ -1,19 +1,33 @@
 package com.technado.neekyu.fragments.home
 
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.technado.neekyu.R
 import com.technado.neekyu.adapters.AppsLockAdapter
 import com.technado.neekyu.base.BaseFragment
 import com.technado.neekyu.databinding.AppsLockFragmentBinding
 import com.technado.neekyu.helper.Titlebar
+import com.technado.neekyu.models.AppModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AppsLockFragment : BaseFragment() {
     var binding: AppsLockFragmentBinding? = null
+    var progRessDialog: ProgressDialog? = null
+    private lateinit var installedAppsList: ArrayList<AppModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,20 +35,47 @@ class AppsLockFragment : BaseFragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_apps_lock, container, false)
 
+        installedAppsList = ArrayList()
         getActivityContext?.lockMenu()
-
+        progRessDialog = ProgressDialog(getActivityContext)
         binding?.recyclerView?.layoutManager = LinearLayoutManager(getActivityContext)
         binding?.recyclerView?.setHasFixedSize(true)
-        binding?.recyclerView?.adapter =
-            AppsLockAdapter(
-                getActivityContext!!,
-                resources.getStringArray(R.array.dummy2).toList()
-            )
 
+        progRessDialog!!.setMessage("Loading...")
+        progRessDialog!!.show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            getInstalledApps()
+            progRessDialog!!.dismiss()
+            binding?.recyclerView?.adapter =
+                AppsLockAdapter(
+                    getActivityContext!!,
+                    getInstalledApps()
+                )
+        }, 500)
         return binding?.root
     }
 
     override fun setTitlebar(titlebar: Titlebar) {
         titlebar.setBackTitle(getActivityContext!!, "Apps")
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun getInstalledApps(): ArrayList<AppModel> {
+        val packs = context?.packageManager?.getInstalledPackages(0)
+        for (i in packs?.indices!!) {
+            val p = packs[i]
+            if (!isSystemPackage(p)) {
+                val appName = p.applicationInfo.loadLabel(context?.packageManager!!).toString()
+                val icon = p.applicationInfo.loadIcon(context?.packageManager!!)
+                val packages = p.applicationInfo.packageName
+                installedAppsList.add(AppModel(appName, icon, packages))
+            }
+        }
+        return installedAppsList
+    }
+
+    private fun isSystemPackage(pkgInfo: PackageInfo): Boolean {
+        return pkgInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0
     }
 }
