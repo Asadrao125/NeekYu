fa76ac5aa3ae37a01bba76d03d772e3e49ce3a3d
asad.muqeem <raoasad125@gmail.com>
2022-01-03 15:23:12 +0500

    Initial Commit

---
+++ app/src/main/java/com/technado/neekyu/fragments/home/DrawPatternFragment.kt
@@ -0,0 +1,31 @@
+package com.technado.neekyu.fragments.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.technado.neekyu.R
+import com.technado.neekyu.base.BaseFragment
+import com.technado.neekyu.databinding.CardDetailsFragmentBinding
+import com.technado.neekyu.databinding.DrawPatternFragmentBinding
+import com.technado.neekyu.helper.Titlebar
+
+class DrawPatternFragment : BaseFragment() {
+    var binding: DrawPatternFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_draw_pattern, container, false)
+
+        return binding?.root
+    }
+
+    override fun setTitlebar(titlebar: Titlebar) {
+        titlebar.setBackTitle(getActivityContext!!, "Draw a pattern")
+    }
+}
