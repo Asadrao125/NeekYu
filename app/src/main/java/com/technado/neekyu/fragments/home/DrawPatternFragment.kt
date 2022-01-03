98ffcedecdf30d85310c52b9fde96f4b67e07363
asad.muqeem <raoasad125@gmail.com>
2022-01-03 18:23:38 +0500

    Initial Commit

--- app/src/main/java/com/technado/neekyu/fragments/home/DrawPatternFragment.kt
+++ app/src/main/java/com/technado/neekyu/fragments/home/DrawPatternFragment.kt
@@ -1,19 +1,12 @@
 package com.technado.neekyu.fragments.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.technado.neekyu.R
-import com.technado.neekyu.adapters.DrawPatternAdapter
-import com.technado.neekyu.adapters.SubscriptionsAdapter
 import com.technado.neekyu.base.BaseFragment
-import com.technado.neekyu.databinding.CardDetailsFragmentBinding
 import com.technado.neekyu.databinding.DrawPatternFragmentBinding
 import com.technado.neekyu.helper.Titlebar
 
@@ -26,14 +19,6 @@ class DrawPatternFragment : BaseFragment() {
     ): View? {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_draw_pattern, container, false)
-
-        binding?.recyclerViewPattern?.layoutManager = GridLayoutManager(getActivityContext, 3)
-        binding?.recyclerViewPattern?.setHasFixedSize(true)
-
-        binding?.recyclerViewPattern?.adapter = DrawPatternAdapter(
-            getActivityContext!!,
-            resources.getStringArray(R.array.pattern).toList()
-        )
 
         getActivityContext!!.lockMenu()
 
